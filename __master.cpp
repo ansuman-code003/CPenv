9fd6d6da9f1e6bf7103af1631b1b553b01e70a74
ansuman-code003 <mazumder.code003@gmail.com>
2023-12-29 19:07:55 +0530

    Small Changes.

--- __master.cpp
+++ __master.cpp
@@ -1,16 +1,1 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-
-void test_case(){
-	/* Solve here! */
-
-}
-
-int main(){
-	int test=1; scanf("%d",&test);
-	for(int i=1;i<=test;i++){
-		test_case();
-	}
-	return 0;
-}
